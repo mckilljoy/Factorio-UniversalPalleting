599967a88aa4be4f7ee4a997fd65adeeeefea05a
Marty Greenia <mckilljoy@gmail.com>
2018-01-23 23:59:31 -0800

    Fix another bobs electronics crafting issue

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,15 +1,2 @@
-data.raw["assembling-machine"]['assembling-machine-2'].crafting_categories = 
-{
-  "crafting",
-  "advanced-crafting",
-  "crafting-with-fluid",
-  "crafting-with-pallets"
-}
-
-data.raw["assembling-machine"]['assembling-machine-3'].crafting_categories = 
-{
-  "crafting",
-  "advanced-crafting",
-  "crafting-with-fluid",
-  "crafting-with-pallets"
-}
+table.insert(data.raw["assembling-machine"]['assembling-machine-2'].crafting_categories, "crafting-with-pallets")
+table.insert(data.raw["assembling-machine"]['assembling-machine-3'].crafting_categories, "crafting-with-pallets")
